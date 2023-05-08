cdb174651182c09bddb8c7986fe24f3e5c2817c0
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-05-08 11:00:17 +0530

    Gave freedom from describe and made it independent

--- test/habit-tracker-test.js
+++ test/habit-tracker-test.js
@@ -1,12 +1,27 @@
 const {describe, it} = require("node:test");
-const {strictEqual, deepStrictEqual} = require("assert");
+const {deepStrictEqual} = require("assert");
 const {initialize} = require("../src/habit-tracker.js");
 
 describe("habit-tracker", function() {
-  const habits = initialize("{}");
-
   describe("add", function() {
-    it("should give a new habit", function() {
+    it("should add a new habit", function() {
+      const habits = initialize("{}");
+      const actual = habits.add("Running", "07 may 2023");
+      const expected = {
+        activity: "Running",
+        startDate: "07 may 2023",
+        streak: 0,
+        showedUp: 0,
+        missed: 0,
+        time: 0,
+      };
+
+      deepStrictEqual(actual, expected);
+    });
+
+    it("should reset a habit if an existing habit is added again", function() {
+      const habits = initialize("{}");
+      const running = habits.add("Running", "07 may 2023");
       const actual = habits.add("Running", "07 may 2023");
       const expected = {
         activity: "Running",
@@ -22,7 +37,9 @@ describe("habit-tracker", function() {
   });
 
   describe("track", function() {
-    it("should add today's record to the respective habit", function() {
+    it("should add today's record to the respective habit and start a streak", function() {
+      const habits = initialize("{}");
+      const running = habits.add("Running", "07 may 2023");
       const actual = habits.track("Running", "showedUp", 30);
       const expected = {
         activity: "Running",
@@ -39,6 +56,7 @@ describe("habit-tracker", function() {
 
   describe("progress", function() {
     it("should give report of a habit", function() {
+      const habits = initialize("{}");
       const expected = {
         activity: "Running",
         startDate: "07 may 2023",
@@ -48,6 +66,8 @@ describe("habit-tracker", function() {
         time: 30,
       };
 
+      habits.add("Running", "07 may 2023");
+      habits.track("Running", "showedUp", 30);
       deepStrictEqual(habits.progress("Running"), expected);
     });
   });
