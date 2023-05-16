394c2d977a94e82d17259b4da9eee9a1c0671b93
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-05-16 10:07:56 +0530

    Remove if-else from streaks function

--- src/habit.js
+++ src/habit.js
@@ -2,36 +2,32 @@ class Habit {
   constructor(activity, habit = {}) {
     this.course = habit.course || []; // []
     this.activity = habit.activity || activity; // "Running"
-    this.startDate = habit.startDate || (new Date()).toDateString(); // "Thu May 12 2023"
+    this.startDate = habit.startDate || new Date().toDateString(); // "Thu May 12 2023"
   }
-  
+
   entry(accomplished, duration) {
     const today = {
-      accomplished: accomplished === 'yes',
+      accomplished: accomplished === "yes",
       duration,
-      timeStamp: (new Date()).toDateString(),
-    }
+      timeStamp: new Date().toDateString(),
+    };
 
     this.course.push(today);
   }
 
   streak() {
-    const initialStreak = { current: 0, largest: 0};
+    const initialStreak = { current: 0, largest: 0 };
 
-    return this.course.reduce(function(streak, day) {
-      if(day.presence) {
-        streak.current += 1;
-      } else {
-        streak.current = 0;
-      }
+    return this.course.reduce(function (streak, day) {
+      streak.current = day.accomplished ? streak.current + 1 : 0;
       streak.largest = Math.max(streak.current, streak.largest);
-      
+
       return streak;
     }, initialStreak);
   }
 
   bestPractice() {
-    return this.course.reduce(function(best, day) {
+    return this.course.reduce(function (best, day) {
       return best.duration > day.duration ? best : day;
     });
   }
--- test/habit-test.js
+++ test/habit-test.js
@@ -1,11 +1,11 @@
-const { describe, it } = require("node:test");
+const { describe, it, run } = require("node:test");
 const { deepStrictEqual } = require("assert");
 const { Habit } = require("../src/habit");
 const { isDeepStrictEqual } = require("util");
 
-describe("Habit", function() {
-  describe("bestPractice", function() {
-    it("should be able to determine best performance till today", function() {
+describe("Habit", function () {
+  describe("bestPractice", function () {
+    it("should be able to determine best performance till today", function () {
       const running = new Habit("running");
       running.entry("yes", 20);
       running.entry("yes", 30);
@@ -13,26 +13,27 @@ describe("Habit", function() {
       const expected = {
         duration: 30,
         accomplished: true,
-        timeStamp: (new Date()).toDateString()
-      }
-      
+        timeStamp: new Date().toDateString(),
+      };
+
       deepStrictEqual(running.bestPractice(), expected);
     });
   });
-  
-  describe("streak", function() {
-    it("should be able to determine largest streak and the current streak", function() {
+
+  describe("streak", function () {
+    it("should be able to determine largest streak and the current streak", function () {
       const running = new Habit("running");
       const expected = {
         current: 1,
-        largest: 3
-      }
-      
-      running.entry(true, 30);
-      running.entry(true, 20);
-      running.entry(true, 30);
-      running.entry(false);
-      running.entry(true, 30);
+        largest: 3,
+      };
+
+      running.entry("yes", 30);
+      running.entry("yes", 20);
+      running.entry("yes", 30);
+      running.entry();
+      running.entry("yes", 30);
+      console.log(running);
       isDeepStrictEqual(running.streak(), expected);
     });
   });
