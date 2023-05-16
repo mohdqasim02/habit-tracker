08630a1ac5646da04e87e01769e0214d18f7110e
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-05-16 11:41:35 +0530

    Update usage message

--- src/habit-tracker.js
+++ src/habit-tracker.js
@@ -8,7 +8,7 @@ class Tracker {
   add(activity) {
     this.habits[activity] = new Habit(activity);
     return `${activity} added for tracking`;
-  };
+  }
 
   remove(activity) {
     delete this.habits[activity];
@@ -18,59 +18,61 @@ class Tracker {
   track(activity, presence, duration) {
     let message = `Today's log added`;
 
-    try{
+    try {
       const habit = this.habits[activity];
       habit.entry(presence, duration);
-    } catch(e) {
+    } catch (e) {
       message = this.errorMsg(activity);
-    };
+    }
 
     return message;
   }
 
   list() {
-    return {...this.habits};
-  };
-
-  activities() {
     return Object.keys(this.habits);
-  };
+  }
 
   progress(activity) {
     try {
       return [...this.habits[activity].course];
-    } catch(e) {
+    } catch (e) {
       return this.errorMsg(activity);
-    };
-  };
-  
+    }
+  }
+
   usage() {
-    let message = '    Usage of tracker.js:';
-    message += '\n -> add activityName';
-    message += '\n -> remove activityName';
-    message += '\n -> track activityName accomplished(yes/no) duration(in mins)';
-    message += '\n -> progress activityName';
-    message += '\n -> activities';
-    message += '\n -> list';
-    
+    let message =
+      "    Usage of tracker.js:" +
+      "\n" +
+      " -> add [activity]" +
+      "\n" +
+      " -> remove [activity]" +
+      "\n" +
+      " -> track [activity] [accomplished: yes/no] [duration in mins]" +
+      "\n" +
+      " -> progress [activity]" +
+      "\n" +
+      " -> list";
+
     return message;
   }
-  
+
   errorMsg(activity) {
-    let error = ` ${activity} habit not found, first add activity to track`
-    error +=  `\n -> usage : node tracker.js add ${activity}`;
+    let error = ` ${activity} habit not found, first add activity to track`;
+    error += `\n -> usage : node tracker.js add ${activity}`;
 
     return error;
   }
 }
 
-const initialize = function(habitsDetails) {
-  const habits = Object.fromEntries(Object.entries(habitsDetails)
-    .map(function([activityName, activityData]) {
+const initialize = function (habitsDetails) {
+  const habits = Object.fromEntries(
+    Object.entries(habitsDetails).map(function ([activityName, activityData]) {
       return [activityName, new Habit(activityName, activityData)];
-    }));
+    })
+  );
 
   return new Tracker(habits);
 };
 
-exports.initialize = initialize;  
+exports.initialize = initialize;
