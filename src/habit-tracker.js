ba577c01627c0f9c566bf3f48fa810866b75c947
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-06-15 14:50:56 +0530

    Refactored: - Change the format of storing data - Add getters

--- src/habit-tracker.js
+++ src/habit-tracker.js
@@ -1,17 +1,19 @@
 const { Habit } = require("./habit.js");
 
 class Tracker {
+  #habits;
+
   constructor(habits) {
-    this.habits = habits;
+    this.#habits = habits;
   }
 
   add(activity) {
-    this.habits[activity] = new Habit(activity);
+    this.#habits[activity] = new Habit(activity);
     return `${activity} added for tracking`;
   }
 
   remove(activity) {
-    delete this.habits[activity];
+    delete this.#habits[activity];
     return `${activity} removed from tracking`;
   }
 
@@ -19,7 +21,7 @@ class Tracker {
     let message = `Today's log added`;
 
     try {
-      const habit = this.habits[activity];
+      const habit = this.#habits[activity];
       habit.entry(presence, duration);
     } catch (e) {
       message = this.errorMsg(activity);
@@ -29,12 +31,12 @@ class Tracker {
   }
 
   list() {
-    return Object.keys(this.habits);
+    return Object.keys(this.#habits);
   }
 
   progress(activity) {
     try {
-      return [...this.habits[activity].course];
+      return this.#habits[activity].course;
     } catch (e) {
       return this.errorMsg(activity);
     }
@@ -63,12 +65,28 @@ class Tracker {
 
     return error;
   }
+
+  get habits() {
+    return Object.values(this.#habits).map((habit) => {
+      return {
+        streaks: habit.streaks,
+        course: habit.course,
+        startDate: habit.startDate,
+        activity: habit.activityName
+      };
+    });
+  }
+
+  getHabit(activityName) {
+    return this.#habits[activityName];
+  }
 }
 
 const initialize = function (habitsDetails) {
   const habits = Object.fromEntries(
-    Object.entries(habitsDetails).map(function ([activityName, activityData]) {
-      return [activityName, new Habit(activityName, activityData)];
+    habitsDetails.map((habit) => {
+      const activityName = habit.activityName;
+      return [activityName, new Habit(activityName, habit)];
     })
   );
 
