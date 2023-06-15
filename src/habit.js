371e8485a7b4b286cd576509e2119b68171467e4
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-06-15 14:49:20 +0530

    Refactored: - Added getters - Made variables private

--- src/habit.js
+++ src/habit.js
@@ -1,26 +1,51 @@
 class Habit {
+  #course;
+  #streaks;
+  #activity;
+  #startDate;
+
   constructor(activity, habit = {}) {
-    this.course = habit.course || []; // []
-    this.activity = habit.activity || activity; // "Running"
-    this.startDate = habit.startDate || new Date().toDateString(); // "Thu May 12 2023"
-    this.streaks = [];
+    this.#course = habit.course || [];
+    this.#streaks = habit.streak || [];
+    this.#activity = habit.activity || activity;
+    this.#startDate = habit.startDate || new Date().toDateString();
   }
 
-  entry(accomplished, duration) {
-    const today = {
+  #createLog(accomplished, duration, otherDetails) {
+    return {
       accomplished: accomplished === "yes",
       duration,
       timeStamp: Date.parse(new Date()),
+      ...otherDetails,
     };
+  }
 
-    this.course.push(today);
-    this.streaks = this.streak();
+  #startNewStreak(streaks, date) {
+    streaks.push({ start: date });
+    return true;
+  }
+
+  #endStreak(streaks, date) {
+    const lastStreak = streaks.at(-1);
+    const daysPassed = (date - lastStreak.start) / (1000 * 60 * 60 * 24);
+
+    lastStreak.end = date;
+    lastStreak.streak = Math.floor(daysPassed);
+
+    return false;
+  }
+
+  entry(accomplished, duration, otherDetails) {
+    const todaysLog = this.#createLog(accomplished, duration, otherDetails);
+
+    this.#course.push(todaysLog);
+    this.#streaks = this.streak();
   }
 
   streak() {
     const streaks = [];
 
-    this.course.reduce((isOnStreak, day) => {
+    this.#course.reduce((isOnStreak, day) => {
       const attempted = day.accomplished;
       const date = day.timeStamp;
 
@@ -36,40 +61,38 @@ class Habit {
     }, false);
 
     return streaks;
-
-
-  }
-
-  #endStreak(streaks, date) {
-    const lastStreak = streaks.at(-1);
-    const daysPassed = (date - lastStreak.start) / (1000 * 60 * 60 * 24);
-
-    lastStreak.end = date;
-    lastStreak.streak = Math.floor(daysPassed);
-
-    return false;
-  }
-
-  #startNewStreak(streaks, date) {
-    streaks.push({ start: date });
-    return true;
   }
 
   currentStreak() {
-    return this.streaks.at(-1);
+    return this.#streaks.at(-1);
   }
 
   longestStreak() {
-    return this.streaks.reduce((longest, streak) => {
+    return this.#streaks.reduce((longest, streak) => {
       return longest.days >= streak.days ? longest : streak;
     });
   }
 
-
   longestDuration() {
-    return this.course.reduce(function (best, day) {
+    return this.#course.reduce((best, day) => {
       return best.duration > day.duration ? best : day;
     });
+  }
+
+  get course() {
+    return [...this.#course];
+  }
+
+  get activityName() {
+    return this.#activity;
+  }
+
+  get startDate() {
+    return this.#startDate;
+  }
+
+  get streaks() {
+    return [...this.#streaks];
   }
 }
 
