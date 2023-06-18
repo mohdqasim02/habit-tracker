5203316c6be1f28e039c4ec2b1e00a2d9affb748
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-06-18 11:57:55 +0530

    Add: Error handling

--- tracker.js
+++ tracker.js
@@ -1,19 +1,27 @@
 const fs = require("fs");
-const {initialize} = require("./src/habit-tracker.js");
+const { Renderer } = require("./src/renderer.js");
+const { Tracker } = require("./src/habit-tracker.js");
+const { parseCommand, parseHabits } = require("./src/parser.js");
 
-const main = function([command, activity, ...args]) {
+const main = function ([command, activity, ...args]) {
   const habitsContent = fs.readFileSync("./resources/habits.json", "utf-8");
-  const tracker = initialize(JSON.parse(habitsContent));
-  let message = '';
 
-  try{
-    message = tracker[command](activity, ...args);
-  } catch(e) {
-    message = `tracker: Not a valid command : ${command}\n`;
-    message += tracker.usage();
+  const habits = parseHabits(habitsContent);
+  const tracker = new Tracker(habits, new Renderer(process.stdout));
+  const { commandToExecute, error } = parseCommand(command);
+
+  if (error) {
+    console.error(error.message);
+    console.error(tracker.usage());
+    process.exit(1);
   }
-  
-  console.log(message);
+
+  try {
+    tracker[commandToExecute](activity, ...args);
+  } catch (err) {
+    console.error(err.message);
+  }
+
   fs.writeFileSync("./resources/habits.json", JSON.stringify(tracker.habits, null, 2));
 };
 
